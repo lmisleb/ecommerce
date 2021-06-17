df46c94a774539fb9c27e6b7e1b419f8d9b086f4
Lenin Misle <lmisleb@gmail.com>
2021-06-17 02:02:34 -0400

    Comenzando gestor ventas

--- backend/vistas/modulos/ventas.php
+++ backend/vistas/modulos/ventas.php
@@ -1,6 +1,6 @@
 <?php
 
-	if($_SESSION["perfil"] != "administrador"){
+	if($_SESSION["perfil"] != "superadministrador" && $_SESSION["perfil"] != "administrador"){
 
 		echo '<script>
 
@@ -39,7 +39,7 @@
 				
 				<?php
 
-					include "inicio/grafico-ventas.php";
+					//include "inicio/grafico-ventas.php";
 
 				?>
 
